d8fb42788d39bec04352a80df587204940398753
mimo31 <viktor.fukala@gmail.com>
2023-01-06 15:11:14 +0100

    Solve 2022 day 4 in Rust

---
+++ rust/2022/4/part1/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let res = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap())
+        .filter(|l| !l.is_empty())
+        .map(|s| s.split(',')
+             .flat_map(|ss| ss.split('-'))
+             .map(|ss| ss.parse::<i32>().unwrap())
+             .collect::<Vec<i32>>())
+        .filter(|li| li[0] <= li[2] && li[3] <= li[1] || li[2] <= li[0] && li[1] <= li[3])
+        .count();
+    println!("{res}");
+}
